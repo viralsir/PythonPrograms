c823e728ff080f80023b8d9e933c28c73f9660f4
ViralSir <viralsir2018@gmail.com>
2023-09-04 17:37:40 +0530

    Initial commit

---
+++ firstdemo.py
@@ -0,0 +1,8 @@
+# for single line comment
+'''   multiline comment   '''
+# print("hello \n\n\n \t\t\t wel come to the world of python")
+print("hello" , end=" , ")
+print("Wel come to the world of python")
+print('python is use for web and data science')
+
+
